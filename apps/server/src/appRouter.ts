1f3cff8cbd76095e1af48f9be1cfee3386b25f8a
Aaron <saarong12@gmail.com>
2026-04-17 01:05:00 -0500

    Removed Sequelize and its usage.

--- apps/server/src/appRouter.ts
+++ apps/server/src/appRouter.ts
@@ -1,18 +1,10 @@
 import { publicProcedure, protectedProcedure, router } from './trpc'
 import { z } from 'zod'
 
-// TODO: DEPRECATE
-import type { User } from '@catalog/types'
-import { UserModel } from './models'
-
 import { prisma } from './lib/prisma'
 
 export const appRouter = router({
   userList: protectedProcedure.query(async () => {
-    // TODO: DEPRECATE.
-    // const userList = await UserModel.findAll()
-    // return userList
-
     const userList = await prisma.user.findMany()
     return userList
   }),
@@ -20,15 +12,6 @@ export const appRouter = router({
     .input(z.object({ id: z.number() }))
     .query(async ({ input }) => {
       const { id } = input
-      // TODO: DEPRECATE.
-      // const user = await UserModel.findOne({
-      //   where: {
-      //     user_id: id,
-      //   },
-      // })
-      // if (!user) return null
-      // return user
-
       const user = await prisma.user.findFirst({
         where: {
           id: id,
@@ -49,14 +32,6 @@ export const appRouter = router({
     )
     .mutation(async (opts) => {
       const { input } = opts
-
-      // TODO: DEPRECATE.
-      // const newUser = await UserModel.create({
-      //   user_id: input.user.user_id,
-      //   name: input.user.name,
-      // })
-
-      // const user = newUser.toJSON() as User
 
       const newUser = await prisma.user.create({
         data: {
