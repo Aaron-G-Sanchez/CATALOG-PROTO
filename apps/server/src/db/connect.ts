9e0f5965f3eb3e2fb90b38a34fa4abd97bcc7bbb
Aaron <saarong12@gmail.com>
2026-04-22 02:36:39 -0500

    Deleted db connection.

--- apps/server/src/db/connect.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import { Sequelize } from 'sequelize'
-
-const DB_CONN_STRING = process.env.DB_CONN_STRING
-
-if (!DB_CONN_STRING) throw new Error('No DB connection string')
-
-export const db = new Sequelize(`postgres://${DB_CONN_STRING}`)
